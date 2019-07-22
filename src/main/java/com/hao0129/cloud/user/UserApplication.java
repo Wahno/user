8c35bd045d2f2e1833f025bbb7a5ee4cb8cc0b75
WANGHAO <1727386617@qq.com>
2019-07-22 15:58:51 +0800

    init project

---
+++ src/main/java/com/hao0129/cloud/user/UserApplication.java
@@ -0,0 +1,13 @@
+package com.hao0129.cloud.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
